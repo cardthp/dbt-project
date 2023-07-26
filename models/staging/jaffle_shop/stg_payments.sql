eb0064f0964c0f7ec27490b1b873c7f068d1359c
Card <thanaphat20@outlook.com>
2023-07-26 11:48:40 +0700

    main

---
+++ models/staging/jaffle_shop/stg_payments.sql
@@ -0,0 +1,9 @@
+select
+    id as payment_id,
+    order_id as order_id,
+    payment_method as payment_method,
+    status,
+    -- amount is stored in cents, convert it to dollars
+    {{ cents_to_dollars('amount', 4) }} as amount,
+    --created as created_at -->>notfound
+from {{ source('jaffle_shop','payments') }}
